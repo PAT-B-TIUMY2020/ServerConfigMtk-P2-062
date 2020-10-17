d80cdf9a5722ee2de645ac4df7616dbbfe73a6ae
Annisa Dian Amarta <anisadiana525@gmail.com>
2020-10-17 14:00:25 +0700

    Add project files.

---
+++ “ServerConfigMtk-P2-20180140062/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ServerConfigMtk_P2_20180140062
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
