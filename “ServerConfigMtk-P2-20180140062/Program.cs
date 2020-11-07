75eb497ca504439df4adaac62dc0a542d86a101e
Annisa Dian Amarta <anisadiana525@gmail.com>
2020-11-07 14:46:47 +0700

    add exception handling

--- “ServerConfigMtk-P2-20180140062/Program.cs
+++ “ServerConfigMtk-P2-20180140062/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140062;
 
 namespace _ServerConfigMtk_P2_20180140062
 {
@@ -10,6 +12,21 @@ namespace _ServerConfigMtk_P2_20180140062
     {
         static void Main(string[] args)
         {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
